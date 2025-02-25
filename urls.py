a3af54eef12b57d1d824d80e885f06896bc5052e
takrim1999 <takrimulalom@gmail.com>
2025-02-25 17:05:37 +0600

    Transfered my guesssing game from github io to here

--- urls.py
+++ urls.py
@@ -4,5 +4,7 @@ from django.urls import include, path
 app_name = 'tgames'
 
 urlpatterns = [
-    path('',include('tgames.calculator.urls')),
+    path('calculator/',include('tgames.calculator.urls')),
+    path('guess/',include('tgames.guess.urls')),
+    
 ]
